0af22b42f3982001085173021eced6b079d99d88
Ajay Jain <Ajay.Jain@azdcs.onmicrosoft.com>
2020-07-28 01:09:47 +0530

    Add project files.

---
+++ Task/Controllers/CustomerController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Task.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+
+            string textFile = @"C:\Ajay\customer.txt";
+            if (File.Exists(textFile))
+            {
+                // Read entire text file content in one string    
+                string text = File.ReadAllText(textFile);
+                Console.WriteLine(text);
+            }
+
+            if (File.Exists(textFile))
+            {
+                // Read a text file line by line.  
+                string[] lines = File.ReadAllLines(textFile);
+                foreach (string line in lines)
+                    Console.WriteLine(line);
+            }
+
+            if (File.Exists(textFile))
+            {
+                // Read file using StreamReader. Reads file line by line  
+                using (StreamReader file = new StreamReader(textFile))
+                {
+                    int counter = 0;
+                    string ln;
+
+                    while ((ln = file.ReadLine()) != null)
+                    {
+                        Console.WriteLine(ln);
+                        counter++;
+                    }
+                    file.Close();
+                    Console.WriteLine("File has {counter} lines.");
+                }
+            }
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Task/Controllers/ValuesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Task.Models;
+
+namespace Task.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        Customer[] products = new Customer[]
+        {
+            new Customer { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+            new Customer { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+            new Customer { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+        };
+        // Default folder    
+
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+        [HttpPost]
+        public IHttpActionResult GetDataFromFile(Customer customer)
+        {
+
+            return Ok();
+
+
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
